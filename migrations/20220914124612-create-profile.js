51a5ad43997e4453844f48971a791435241cea44
RizkyFadhilla <rizkyfadhilla123@gmail.com>
2022-09-14 20:08:59 +0700

    update sementara

---
+++ migrations/20220914124612-create-profile.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Profiles', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      fullName: {
+        type: Sequelize.STRING
+      },
+      dateOfBirth: {
+        type: Sequelize.DATE
+      },
+      adress: {
+        type: Sequelize.STRING
+      },
+      background: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Profiles');
+  }
+};
---
+++ models/users_course.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Users_Course extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Users_Course.init({
+    UserId: DataTypes.INTEGER,
+    CourseId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Users_Course',
+  });
+  return Users_Course;
+};
